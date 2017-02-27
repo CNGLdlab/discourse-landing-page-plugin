702bceac5d0f128801c483a54f717a40a3e8be7e
Daniel Turner <dturner@tcd.ie>
2017-02-27 11:22:59 +0000

    add plugin file

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: landing-page
+# about: Plugin that injects a custom landing-page into the discourse instance
+# version: 0.0.1
+# authors: Daniel Turner daniel.turner@adaptcentre.ie
+
+register_asset "stylesheets/landing-page.css"
